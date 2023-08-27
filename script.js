ad1c87d17da76376ce5251d11611f5fa0eb97304
rainygodz <120272656+rainygodz@users.noreply.github.com>
2023-08-27 19:21:56 +0400

    feat: implement addBookToList and updateBookList functions

--- script.js
+++ script.js
@@ -3,14 +3,66 @@ const modalWindow = document.querySelector("dialog");
 
 const myBookList = [];
 
-function Book(title, author, pages, read="not read yet") {
+function Book(title, author, pages, readStatus) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.readStatus = readStatus;
 }
 
+
+function addBookToList(title, author, pages, readStatus) {
+  const book = new Book(title, author, pages, readStatus);
+  myBookList.push(book);
+}
+
+function updateBookList() {
+  const container = document.querySelector(".container");
+  const bookCardDiv = document.createElement("div");
+  const infoDiv = document.createElement("div");
+  const titleDiv = document.createElement("div");
+  const authorDiv = document.createElement("div");
+  const pagesDiv = document.createElement("div");
+  const readDiv = document.createElement("div");
+  const buttonsDiv = document.createElement("div");
+  const readBtn = document.createElement("button");
+  const removeBtn = document.createElement("button");
+
+  bookCardDiv.classList.add("book-card");
+  infoDiv.classList.add("info");
+  titleDiv.classList.add("title");
+  authorDiv.classList.add("author");
+  pagesDiv.classList.add("pages");
+  readDiv.classList.add("read");
+  buttonsDiv.classList.add("buttons");
+  readBtn.setAttribute("id", "read");
+  removeBtn.setAttribute("id", "remove");
+
+  titleDiv.textContent = myBookList[0].title;
+  authorDiv.textContent = myBookList[0].author;
+  pagesDiv.textContent = "Pages: " + myBookList[0].pages;
+  readDiv.textContent = myBookList[0].readStatus;
+  readBtn.textContent = "Read";
+  removeBtn.textContent = "Remove";
+
+  buttonsDiv.appendChild(readBtn);
+  buttonsDiv.appendChild(removeBtn);
+
+  infoDiv.appendChild(titleDiv);
+  infoDiv.appendChild(authorDiv);
+  infoDiv.appendChild(pagesDiv);
+  infoDiv.appendChild(readDiv);
+
+  bookCardDiv.appendChild(infoDiv);
+  bookCardDiv.appendChild(buttonsDiv);
+
+  container.appendChild(bookCardDiv);
+}
+
+
 openModalBtn.addEventListener("click", () => {
-  modalWindow.style.display = "visible"
   modalWindow.showModal();
 });
+
+addBookToList("The Financier", "Theodore Dreiser", 230, "not read yet");
+updateBookList();
