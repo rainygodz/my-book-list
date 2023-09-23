fad724bc46f5ab1510c3ffb6b96126e0f7f8b6f2
rainygodz <120272656+rainygodz@users.noreply.github.com>
2023-09-23 15:35:00 +0400

    feat: add form validation

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const openModalBtn = document.querySelector("#open-modal");
-const modalWindow = document.querySelector("dialog");
-const addBookBtn = document.querySelector("#add-book");
-
+const openModalBtn = document.querySelector('#open-modal');
+const modalWindow = document.querySelector('dialog');
+const addBookBtn = document.querySelector('#add-book');
 
 const myBookList = [];
-
 
 class Book {
   constructor(title, author, pages, readStatus) {
@@ -15,108 +13,116 @@ class Book {
   }
 }
 
-
 function addBookToList() {
-  const form = document.querySelector("form");
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  const pages = document.querySelector("#pages").value;
-  const readStatus = document.querySelector("#read-status").value;
+  const form = document.querySelector('form');
+  const titleInput = document.querySelector('#title');
+  const authorInput = document.querySelector('#author');
+  const pagesInput = document.querySelector('#pages');
+  const readStatus = document.querySelector('#read-status').value;
 
-  const book = new Book(title, author, pages, readStatus);
-  myBookList.push(book);
-  form.reset();
+  if (
+    titleInput.checkValidity() &&
+    authorInput.checkValidity() &&
+    pagesInput.checkValidity()
+  ) {
+    const book = new Book(
+      titleInput.value,
+      authorInput.value,
+      pagesInput.value,
+      readStatus
+    );
+    myBookList.push(book);
+    modalWindow.close();
+    form.reset();
+  }
 }
 
 function updateBookList() {
-  const container = document.querySelector(".container");
-  container.innerHTML = "";
+  const container = document.querySelector('.container');
+  container.innerHTML = '';
   myBookList.forEach((book, index) => {
-    const bookCardDiv = document.createElement("div");
-  const infoDiv = document.createElement("div");
-  const titleDiv = document.createElement("div");
-  const authorDiv = document.createElement("div");
-  const pagesDiv = document.createElement("div");
-  const readDiv = document.createElement("div");
-  const buttonsDiv = document.createElement("div");
-  const readBtn = document.createElement("button");
-  const removeBtn = document.createElement("button");
+    const bookCardDiv = document.createElement('div');
+    const infoDiv = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    const authorDiv = document.createElement('div');
+    const pagesDiv = document.createElement('div');
+    const readDiv = document.createElement('div');
+    const buttonsDiv = document.createElement('div');
+    const readBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
 
-  bookCardDiv.classList.add("book-card");
-  infoDiv.classList.add("info");
-  titleDiv.classList.add("title");
-  authorDiv.classList.add("author");
-  pagesDiv.classList.add("pages");
-  readDiv.classList.add("read");
-  buttonsDiv.classList.add("buttons");
-  readBtn.setAttribute("id", "read");
-  readBtn.setAttribute("book-id", `${index}`);
-  removeBtn.setAttribute("id", "remove");
-  removeBtn.setAttribute("book-id", `${index}`);
+    bookCardDiv.classList.add('book-card');
+    infoDiv.classList.add('info');
+    titleDiv.classList.add('title');
+    authorDiv.classList.add('author');
+    pagesDiv.classList.add('pages');
+    readDiv.classList.add('read');
+    buttonsDiv.classList.add('buttons');
+    readBtn.setAttribute('id', 'read');
+    readBtn.setAttribute('book-id', `${index}`);
+    removeBtn.setAttribute('id', 'remove');
+    removeBtn.setAttribute('book-id', `${index}`);
 
-  titleDiv.textContent = book.title;
-  authorDiv.textContent = book.author;
-  pagesDiv.textContent = "Pages: " + book.pages;
-  readDiv.textContent = book.readStatus;
-  readBtn.textContent = "Read";
-  removeBtn.textContent = "Remove";
+    titleDiv.textContent = book.title;
+    authorDiv.textContent = book.author;
+    pagesDiv.textContent = 'Pages: ' + book.pages;
+    readDiv.textContent = book.readStatus;
+    readBtn.textContent = 'Read';
+    removeBtn.textContent = 'Remove';
 
-  buttonsDiv.appendChild(readBtn);
-  buttonsDiv.appendChild(removeBtn);
+    buttonsDiv.appendChild(readBtn);
+    buttonsDiv.appendChild(removeBtn);
 
-  infoDiv.appendChild(titleDiv);
-  infoDiv.appendChild(authorDiv);
-  infoDiv.appendChild(pagesDiv);
-  infoDiv.appendChild(readDiv);
+    infoDiv.appendChild(titleDiv);
+    infoDiv.appendChild(authorDiv);
+    infoDiv.appendChild(pagesDiv);
+    infoDiv.appendChild(readDiv);
 
-  bookCardDiv.appendChild(infoDiv);
-  bookCardDiv.appendChild(buttonsDiv);
+    bookCardDiv.appendChild(infoDiv);
+    bookCardDiv.appendChild(buttonsDiv);
 
-  container.appendChild(bookCardDiv);
-  })
+    container.appendChild(bookCardDiv);
+  });
 
   if (!container.innerHTML) {
-    const emptyMessage = document.createElement("h1");
-    emptyMessage.textContent = "No books in your list yet.";
-    emptyMessage.classList.add("empty-message");
+    const emptyMessage = document.createElement('h1');
+    emptyMessage.textContent = 'No books in your list yet.';
+    emptyMessage.classList.add('empty-message');
     container.appendChild(emptyMessage);
   }
 
-  const readBtns = document.querySelectorAll("#read");
-  readBtns.forEach(button => {
-    button.addEventListener("click", changeReadStatus);
+  const readBtns = document.querySelectorAll('#read');
+  readBtns.forEach((button) => {
+    button.addEventListener('click', changeReadStatus);
   });
 
-  const removeBtns = document.querySelectorAll("#remove");
-  removeBtns.forEach(button => {
-    button.addEventListener("click", removeBook);
-  })
+  const removeBtns = document.querySelectorAll('#remove');
+  removeBtns.forEach((button) => {
+    button.addEventListener('click', removeBook);
+  });
 }
 
 function changeReadStatus(event) {
-  const bookId = event.target.getAttribute("book-id");
+  const bookId = event.target.getAttribute('book-id');
 
-  if (myBookList[bookId].readStatus === "Read") {
-    myBookList[bookId].readStatus = "Not read yet";
+  if (myBookList[bookId].readStatus === 'Read') {
+    myBookList[bookId].readStatus = 'Not read yet';
     updateBookList();
   } else {
-    myBookList[bookId].readStatus = "Read"
+    myBookList[bookId].readStatus = 'Read';
     updateBookList();
   }
 }
 
-
 function removeBook(event) {
-  const bookId = event.target.getAttribute("book-id");
+  const bookId = event.target.getAttribute('book-id');
   myBookList.splice(bookId, 1);
   updateBookList();
 }
 
-
-openModalBtn.addEventListener("click", () => {
+openModalBtn.addEventListener('click', () => {
   modalWindow.showModal();
 });
 
-addBookBtn.addEventListener("click", addBookToList);
-addBookBtn.addEventListener("click", updateBookList);
-
+addBookBtn.addEventListener('click', addBookToList);
+addBookBtn.addEventListener('click', updateBookList);
